b23707a7a35052447d3c869bbcb6cede1e07d612
puss <673256903@qq.com>
2020-02-26 17:29:09 +0800

    增加API操作

---
+++ 3.Api/WL.Api.Home/Controllers/ApiBaseController.cs
@@ -0,0 +1,133 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Claims;
+using System.Text;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WL.API.Models;
+using WL.Infrastructure.Commom;
+
+namespace WL.API.Controllers
+{
+
+    public class ApiBaseController : ApiController
+    {
+        /// <summary>
+        /// HttpResponse直接返回Json结果
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        protected virtual HttpResponseMessage Json(string json)
+        {
+            var response = this.Request.CreateResponse(HttpStatusCode.OK);
+            response.Content = new StringContent(json, Encoding.UTF8, "application/json");
+            return response;
+        }
+
+        private string requestBody = "";
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public ResponseData<T> InvokeFunc<T>(Func<T> func)
+        {
+            Stopwatch stop = new Stopwatch();
+            stop.Start();
+            var response = new ResponseData<T>();
+            try
+            {
+                //使用期限
+                // UsingTimeOut();
+                requestBody = GetPostData();
+                T result = func();
+                response.data = result;
+
+            }
+            catch (AppException exception)
+            {
+                response.HandleException(exception);
+                //ILog log = LogManager.GetLogger("AppException");
+                //log.Warn(exception.Message, exception);
+            }
+            catch (Exception exception)
+            {
+                //记录数据库日志
+                #region 日志记录
+                //string errorCode = DateTime.Now.ToString("yyMMddHHmmss");
+                //ExceptionLog exceptionLog = new ExceptionLog();
+                //exceptionLog.Response = JsonConvert.SerializeObject(exception);
+                //exceptionLog.Error_code = errorCode;
+                //exceptionLog.Exception_Type = (int)ExceptionTypeEnum.System;
+                //exceptionLog.Url = Url;
+                //exceptionLog.CreateTime = DateTime.Now;
+                //exceptionLog.Request = requestBody;
+                response.message = "网络异常或超时，请稍后再试！";// string.Format("系统异常,错误代码：{0}", errorCode);
+                //ExceptionLogDsService.Log(exceptionLog);
+                #endregion
+
+                //ILog log = LogManager.GetLogger("exception");
+                //log.Error(exception.Message, exception);
+            }
+
+            stop.Stop();
+            long runtime = stop.ElapsedMilliseconds;
+            //ExceptionLogDsService.TimeOutLog(Url, requestBody, JsonConvert.SerializeObject(response), runtime);
+
+            return response;
+        }
+
+        /// <summary>
+        /// 获取POST传过来的数据
+        /// </summary>
+        /// <returns></returns>
+        protected string GetPostData()
+        {
+            try
+            {
+                HttpRequest rq = HttpContext.Current.Request;
+                return getResultData(rq.InputStream);
+            }
+            catch (Exception xe)
+            {
+                return "";
+            }
+
+        }
+        private string getResultData(Stream task)
+        {
+            string taskData = string.Empty;
+            using (System.IO.Stream sm = task)
+            {
+                if (sm != null)
+                {
+                    sm.Seek(0, SeekOrigin.Begin);
+                    int len = (int)sm.Length;
+                    byte[] inputByts = new byte[len];
+                    sm.Read(inputByts, 0, len);
+                    sm.Close();
+                    taskData = Encoding.UTF8.GetString(inputByts);
+                }
+            }
+            return taskData;
+        }
+        /// <summary>
+        /// 获取请求url
+        /// </summary>
+        protected string Url
+        {
+            get
+            {
+                if (Request == null || Request.RequestUri == null)
+                {
+                    return string.Empty;
+                }
+                return Request.RequestUri.ToString();
+            }
+        }
+
+    }
+}
---
+++ 3.Api/WL.Api.Home/Models/ResponseData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WL.Infrastructure.Commom;
+
+namespace WL.API.Models
+{
+    public class ResponseData<T>
+    {
+        /// <summary>
+        /// 状态 0表示正常
+        /// </summary>
+        public int status { get; set; }
+        public string message { get; set; }
+        public T data { get; set; }
+
+
+        public ResponseData() { }
+
+        public ResponseData(T data)
+        {
+            this.data = data;
+        }
+
+        public void HandleException(Exception ex)
+        {
+            this.status = (int)ReturnResultStatus.BLLError;
+            this.message = ex.Message;
+        }
+
+        public void HandleException(AppException ex)
+        {
+            this.status = ex.ErrorStatus;
+            this.message = ex.Message;
+        }
+
+    }
+}
