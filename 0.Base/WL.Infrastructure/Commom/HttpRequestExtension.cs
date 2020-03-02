3738db499082fe7315490df893d100d4314d6e23
韦俊宇 <673256903@qq.com>
2020-03-02 23:04:41 +0800

    IsAjaxRequest

---
+++ 0.Base/WL.Infrastructure/Commom/HttpRequestExtension.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WL.Infrastructure.Common
+{
+    public static class HttpRequestExtension
+    {
+        public static bool IsAjaxRequest(HttpRequestBase request)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+            return (request.Headers["X-Requested-With"] == "XMLHttpRequest") || ((request.Headers != null) && (request.Headers["X-Requested-With"] == "XMLHttpRequest"));
+        }
+    }
+}
